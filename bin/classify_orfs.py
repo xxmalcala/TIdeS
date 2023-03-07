0d67ea06bca800b473ac44e0fa0d874cc6017df7
XXMA <xyrus.malcala@gmail.com>
2023-03-07 11:57:38 -0500

    Upload ML-classifying script.

---
+++ bin/classify_orfs.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+
+import pickle, time
+import pandas as pd
+
+from datetime import timedelta
+from numpy import argmax
+from pathlib import Path
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import (train_test_split, GridSearchCV)
+
+
+def extract_features(feature_dict: dict, contam: bool, training: bool):
+    feature_df = pd.DataFrame.from_dict(feature_dict, orient = 'index')
+    if training:
+        if contam:
+            y_ref_df = feature_df.index.map(lambda x: 1 if x.split('_')[-1] == 'Target' else 0)
+
+        else:
+            y_ref_df = feature_df.index.map(lambda x: 1 if x[-1] == '1' else 0)
+
+        return feature_df, y_ref_df
+
+    return feature_df
+
+
+
+def train_rfc(X_ref_df, y_ref_df, threads: int):
+    X_train, X_test, y_train, y_test = train_test_split(X_ref_df,
+                                                        y_ref_df,
+                                                        test_size = 0.2,
+                                                        random_state = 42,
+                                                        stratify = y_ref_df)
+
+    param_grid = {
+        'n_estimators': [300, 500],
+        'max_features': ['sqrt', 'log2'],
+        'min_samples_split': [2, 5],
+        'max_depth' : [4, 8, 12, 16],
+        'criterion' :['gini', 'entropy'],
+        'oob_score': [True],
+        'n_jobs': [int(threads)]}
+
+    CV_rfc = GridSearchCV(
+        estimator = RandomForestClassifier(random_state = 42),
+        param_grid = param_grid,
+        cv = 5,
+        n_jobs = int(threads))
+
+    CV_rfc.fit(X_train, y_train)
+    rfc = RandomForestClassifier(random_state = 42, **CV_rfc.best_params_)
+
+    rfc.fit(X_train, y_train)
+
+    return rfc
+
+
+def save_rfc(rfc_model, txn_code):
+    rfc_pkl = f'{txn_code}_TIdeS/{txn_code}.TIdeS.pkl'
+    pickle.dump(rfc_model, open(rfc_pkl, 'wb+'))
+
+
+def process_predictions(txn_code: str, query_preds, query_df: pd.core.frame.DataFrame, contam: bool) -> dict:
+
+    tds_dir = f'{txn_code}_TIdeS/Classified/'
+    Path(tds_dir).mkdir(parents = True, exist_ok = True)
+
+    pos, neg = 'CRF', 'OOF'
+    if contam:
+        pos, neg = 'Target', 'Non-Target'
+
+    pos_pred_seqs = []
+    contam_seqs = []
+
+    tds_tsv = f'{tds_dir}{txn_code}.TIdeS_Summary.tsv'
+    # tds_fas = f'{tds_dir}{txn_code}.TIdeS.fas'
+    if contam:
+        tds_contam = f'{tds_dir}{txn_code}.TIdeS.NonTargetSeqs.fas'
+
+    with open(tds_tsv, 'w+') as w:
+        w.write(f'Gene\tTIdes_Prediction\tProb_{neg}\tProb_{pos}\n')
+        for n in range(len(query_preds)):
+            if argmax(query_preds[n]) == 0:
+                w.write(f'{query_df.index[n]}\t{neg}\t'
+                        f'{query_preds[n][0]*100:.2f}\t'
+                        f'{query_preds[n][1]*100:.2f}\n')
+                if contam:
+                    contam_seqs.append(query_df.index[n])
+
+            else:
+                pos_pred_seqs.append(query_df.index[n])
+                w.write(f'{query_df.index[n]}\t{pos}\t'
+                        f'{query_preds[n][0]*100:.2f}\t'
+                        f'{query_preds[n][1]*100:.2f}\n')
+
+    if contam:
+        with open(tds_contam, 'w+') as w:
+            for k, v in contam_seqs.items():
+                w.write(f'{k}\n{v}\n')
+        return pos_pred_seqs, contam_seqs
+
+    return pos_pred_seqs
+
+
+def classify_orfs(txn_code: str,
+                    train_orfs: dict,
+                    query_orfs: dict,
+                    stp_cdns: list,
+                    start_time,
+                    pretrained: str = None,
+                    threads: int = 4,
+                    verb: bool = True,
+                    contam: bool = False) -> dict:
+
+    if verb:
+        print(f'[{timedelta(seconds=round(time.time()-start_time))}]  Extracting training features')
+
+    query_df = extract_features(query_orfs, contam, False)
+
+    if not pretrained:
+        X_ref_df, y_ref_df = extract_features(train_orfs, contam, True)
+
+        if verb:
+            print(f'[{timedelta(seconds=round(time.time()-start_time))}]  Training TIdeS')
+
+        trained_rfc = train_rfc(X_ref_df, y_ref_df, threads)
+
+    else:
+        trained_rfc = pickle.load(open(pretrained, 'rb'))
+
+    save_rfc(trained_rfc, txn_code)
+
+    if verb:
+        print(f'[{timedelta(seconds=round(time.time()-start_time))}]  Classifying ORFs')
+
+    query_preds = trained_rfc.predict_proba(query_df)
+
+    if contam:
+        target_seqs, contam_seqs =  process_predictions(txn_code, query_preds, query_df, contam)
+
+        return target_seqs, contam_seqs
+
+    else:
+        crf_seqs = process_predictions(txn_code, query_preds, query_df, contam)
+
+        return crf_seqs
+    # return process_predictions(txn_code, query_preds, query_df, contam)
+
+#
+# def score_hits(query_preds, query_df):
+#     comp_hits = {i.split('\t')[0]:int(i.split('\t')[-1]) for i in open('/home/dr_x/Desktop/New_TIdeS/Am_my_Ddis.300bp.CompORFs.RefGenome.tsv').readlines()}
+#     potential_crf = len([k for k, v in comp_hits.items() if v == 1])
+#     qpn = {}
+#     for n in range(len(query_preds)):
+#         qpn[query_df["Seq_Name"][n]] = argmax(query_preds[n])
+#     tds_pred = len([k for k, v in qpn.items() if v == 1])
+#     n_with_hit = len([k for k, v in qpn.items() if k in comp_hits.keys() and v == 1])
+#     crf_with_hit = len([k for k, v in qpn.items() if k in comp_hits.keys() and v == 1 and comp_hits[k] == 1])
+#     print(f'Potential-CRF-Hits: {potential_crf}')
+#     print(f'Total-TIdeS-pORF-Preds: {tds_pred}')
+#     print(f'Total-TIdeS-pORF-Hits: {n_with_hit}')
+#     print(f'Total-TIdeS-pORF-CRF: {crf_with_hit}')
+#
+# def plot_roc():
+#     import matplotlib.pyplot as plt
+#     from sklearn.metrics import RocCurveDisplay
+#     rfc_roc = RocCurveDisplay.from_estimator(rfc, X_test, y_test)
+#     plt.show()
