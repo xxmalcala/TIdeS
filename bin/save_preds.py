38ece997b3c278abe1684e075722edf5b46f7a71
XXMA <xyrus.malcala@gmail.com>
2023-03-14 17:05:28 -0400

    Minor fixes.

--- bin/save_preds.py
+++ bin/save_preds.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python3
 
-import shutil
+import shutil, sys
 from Bio import SeqIO
+from collections import defaultdict
 
 from bin import orf_call as oc
 
-def finalize_outputs(txn_code: str,  ref_orf_fas: str, imp_orfs: list, minor_orfs: list, gcode: str = '1', min_len: int = 300, contam = False):
+def finalize_outputs(
+                    txn_code: str,
+                    ref_orf_fas: str,
+                    imp_orfs: list,
+                    minor_orfs: list,
+                    gcode: str = '1',
+                    min_len: int = 300,
+                    contam = False) -> None:
     if contam:
         imp_orf_fas = f'{txn_code}_TIdeS/Classified/{txn_code}.TIdeS.fas'
-        mnr_orf_fas = f'{txn_code}_TIdeS/Classified/{txn_code}.NonTarget.TIdeS.fas'
+        mnr_orf_fas = f'{txn_code}_TIdeS/Classified/{txn_code}.TIdeS.NonTarget.fas'
+
     else:
         imp_orf_fas = f'{txn_code}_TIdeS/Classified/{txn_code}.TIdeS.fas'
-        mnr_orf_fas = f'{txn_code}_TIdeS/Classified/{txn_code}.TIdeS.AllPreds.fas'
 
     tds_ps = {}
 
     ref_dict = {i.id:i for i in SeqIO.parse(ref_orf_fas, 'fasta')}
 
     with open(imp_orf_fas, 'w+') as w:
-        for i in imp_orfs:
-            w.write(f'>{ref_dict[i].description}\n{ref_dict[i].seq}\n')
-            tds_ps[ref_dict[i].description] = f'{ref_dict[i].seq}'
+        if contam:
+            for i in imp_orfs:
+                w.write(f'>{ref_dict[i].description}\n{ref_dict[i].seq}\n')
+                tds_ps[ref_dict[i].description] = f'{ref_dict[i].seq}'
 
-    with open(mnr_orf_fas, 'w+') as w:
-        for i in minor_orfs:
-            w.write(f'>{ref_dict[i].description}\n{ref_dict[i].seq}\n')
+            with open(mnr_orf_fas, 'w+') as w:
+                for i in minor_orfs:
+                    w.write(f'>{ref_dict[i].description}\n{ref_dict[i].seq}\n')
+        else:
+            top_orfs = filter_tides_preds(ref_dict, imp_orfs)
+            for i in top_orfs:
+                w.write(f'>{i.description}\n{i.seq}\n')
+                tds_ps[i.description] = f'{i.seq}'
 
     with open(imp_orf_fas.replace("fas","AA.fas"), 'w+') as w:
         tds_psa = oc.translate_seqs(tds_ps, gcode)
@@ -33,3 +47,19 @@ def finalize_outputs(txn_code: str,  ref_orf_fas: str, imp_orfs: list, minor_orf
 
     shutil.copy2(imp_orf_fas, f'{txn_code}_TIdeS/')
     shutil.copy2(imp_orf_fas.replace("fas","AA.fas"), f'{txn_code}_TIdeS/')
+
+
+def filter_tides_preds(ref_dict: dict, imp_orfs: list) -> list:
+    tmp_dict = defaultdict(list)
+    for k, v in ref_dict.items():
+        if k in imp_orfs:
+            tmp_dict[k.split('.pORF')[0]].append(v)
+
+    top_orfs = []
+    for k, v in tmp_dict.items():
+        if len(v) > 1:
+            maxlen = max([len(i) for i in v])
+            top_orfs += [i for i in v if len(i) == maxlen]
+        else:
+            top_orfs += v
+    return top_orfs
