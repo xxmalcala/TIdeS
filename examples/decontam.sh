5528cd29a9bcfca2c32658e3af87f52806b9e95b
XXMA <xyrus.malcala@gmail.com>
2023-09-05 13:03:08 -0600

    Fixed typo

--- examples/decontam.sh
+++ examples/decontam.sh
@@ -1,3 +1,3 @@
-tar -zxvf *tar.gz
+tar -zxvf Mesodinium.Cryptophyte.fasta.tar.gz
 
 python3 ../tides.py -f Mesodinium.Cryptophyte.fasta -n Mesodinium_Cryptophyte_Example -c Mesodinium.Cryptophyte.txt -t 1 -gz
